f74ef93a630979243db5d82aff790436f549df45
shimon <69175188+lil-shimon@users.noreply.github.com>
2022-05-24 16:01:04 +0900

    feat: 🎸 moving from callbacks to coroutines

--- start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
+++ start/src/main/java/com/example/android/kotlincoroutines/main/TitleRepository.kt
@@ -19,6 +19,7 @@ package com.example.android.kotlincoroutines.main
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.map
 import com.example.android.kotlincoroutines.util.BACKGROUND
+import kotlinx.coroutines.delay
 
 /**
  * TitleRepository provides an interface to fetch a title or request a new one be generated.
@@ -72,6 +73,10 @@ class TitleRepository(val network: MainNetwork, val titleDao: TitleDao) {
                         TitleRefreshError("Unable to refresh title", cause))
             }
         }
+    }
+
+    suspend fun refreshTitle() {
+        delay(500)
     }
 }
 
